b13d2200e67340dab1837ed58588efb4cfaed158
Steve Cherry <cherrys@cua.edu>
2014-03-02 15:17:15 -0500

    Display the answer instead of the data

--- app/scripts/views/app.js
+++ app/scripts/views/app.js
@@ -18,9 +18,21 @@ define([
         },
 
         render: function () {
-            $(this.el).html(this.template({ high: this.collection.high(), low: this.collection.low(), precip: this.collection.precip() }));
+            $('body').removeClass('yes no maybe').addClass(this.answer());
+
+            this.$el.html(this.template({ answer: this.answer() }));
 
             return this;
+        },
+
+        answer: function () {
+            if (this.collection.high() <= 90 && this.collection.low() >= 50 && this.collection.precip() <= 10) {
+                return 'yes';
+            } else if (this.collection.high() <= 90 + 10 && this.collection.low() >= 50 - 10 && this.collection.precip() <= 10 + 10) {
+                return 'maybe';
+            } else {
+                return 'no';
+            }
         }
     });
 
