069b27b713c914f87286e8615a4d1451cce0db9e
Steve Cherry <cherrys@cua.edu>
2014-06-04 10:53:41 -0400

    Add debuging code

--- app/scripts/models/answer.js
+++ app/scripts/models/answer.js
@@ -28,6 +28,8 @@ define([
         },
 
         decide: function (data) {
+            console.info('h: ' + data.high() + ', l: ' + data.low() + ', p: ' + data.precip());
+
             if (data.high() <= this.get('high') && data.low() >= this.get('low') && data.precip() <= this.get('precip')) {
                 this.set({ answer: 'yes' });
             } else if (data.high() <= this.get('high') + this.get('offset') && data.low() >= this.get('low') - this.get('offset') && data.precip() <= this.get('precip') + this.get('offset')) {
